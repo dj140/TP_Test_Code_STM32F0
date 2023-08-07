5f1c6305925b2a32145ed84affd62f5dff8a1c4e
dj140 <1613270283@qq.com>
2023-08-08 02:04:44 +0800

    clean

--- MDK-ARM/Objects/itoa.d
+++
@@ -1,3 +0,0 @@
-.\objects\itoa.o: ..\ArduinoAPI\itoa.c
-.\objects\itoa.o: ..\ArduinoAPI\itoa.h
-.\objects\itoa.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
--- MDK-ARM/Objects/startup_stm32f0xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f0xx.o: ..\Core\CMSIS\Device\ST\STM32F0xx\Source\Templates\arm\startup_stm32f0xx.s
